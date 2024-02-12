cc649d2e91f30932eb98e9d93105d68364aeaf07
Dennis Chen <denchen@yahooinc.com>
2024-02-12 12:54:50 -0800

    adf

--- types/index.test-d.ts
+++ types/index.test-d.ts
@@ -1,24 +1,24 @@
-import { fastify, FastifyInstance, FastifyServerOptions } from "fastify";
-import { expectAssignable, expectType } from "tsd";
-import { restartable, ApplicationFactory } from "./index";
-import type { Http2Server } from "http2";
+import { fastify, FastifyInstance, FastifyServerOptions } from 'fastify'
+import { expectAssignable, expectType } from 'tsd'
+import { restartable, ApplicationFactory } from './index'
+import type { Http2Server } from "http2"
 
-type Fastify = typeof fastify;
+type Fastify = typeof fastify
 
-async function createApplication(
+async function createApplication (
   fastify: Fastify,
   opts: FastifyServerOptions,
   restartOpts?: unknown
 ): Promise<FastifyInstance> {
-  const app = fastify(opts);
+  const app = fastify(opts)
 
-  expectAssignable<FastifyInstance>(app);
-  expectAssignable<unknown>(restartOpts);
+  expectAssignable<FastifyInstance>(app)
+  expectAssignable<unknown>(restartOpts)
 
-  expectType<boolean>(app.restarted);
-  expectType<(restartOpts?: unknown) => Promise<void>>(app.restart);
+  expectType<boolean>(app.restarted)
+  expectType<(restartOpts?: unknown) => Promise<void>>(app.restart)
 
-  return app;
+  return app
 }
 
 // This currently fails with:
@@ -28,30 +28,30 @@ async function createApplication(
 // opts: FastifyServerOptions, restartOpts?: unknown)
 // => Promise<FastifyInstance<RawServerDefault, IncomingMessage, ServerResponse<...>,
 // FastifyBaseLogger, FastifyTypeProviderDefault>>
-// expectType<ApplicationFactory>(createApplication);
+// expectType<ApplicationFactory>(createApplication)
 
 {
-  const app = await restartable(createApplication);
-  expectType<FastifyInstance>(app);
-  expectType<boolean>(app.restarted);
-  expectType<boolean>(app.closingRestartable);
-  expectType<(restartOpts?: unknown) => Promise<void>>(app.restart);
+  const app = await restartable(createApplication)
+  expectType<FastifyInstance>(app)
+  expectType<boolean>(app.restarted)
+  expectType<boolean>(app.closingRestartable)
+  expectType<(restartOpts?: unknown) => Promise<void>>(app.restart)
 }
 
 {
-  const app = await restartable(createApplication, { logger: true });
-  expectType<FastifyInstance>(app);
-  expectType<boolean>(app.restarted);
-  expectType<boolean>(app.closingRestartable);
-  expectType<(restartOpts?: unknown) => Promise<void>>(app.restart);
+  const app = await restartable(createApplication, { logger: true })
+  expectType<FastifyInstance>(app)
+  expectType<boolean>(app.restarted)
+  expectType<boolean>(app.closingRestartable)
+  expectType<(restartOpts?: unknown) => Promise<void>>(app.restart)
 }
 
 {
-  const app = await restartable(createApplication, { logger: true }, fastify);
-  expectType<FastifyInstance>(app);
-  expectType<boolean>(app.restarted);
-  expectType<boolean>(app.closingRestartable);
-  expectType<(restartOpts?: unknown) => Promise<void>>(app.restart);
+  const app = await restartable(createApplication, { logger: true }, fastify)
+  expectType<FastifyInstance>(app)
+  expectType<boolean>(app.restarted)
+  expectType<boolean>(app.closingRestartable)
+  expectType<(restartOpts?: unknown) => Promise<void>>(app.restart)
 }
 
 {
@@ -59,19 +59,15 @@ async function createApplication(
     async (factory, opts) => await factory(opts),
     { http2: true },
     fastify
-  );
-  expectType<FastifyInstance<Http2Server>>(app);
-  expectType<boolean>(app.restarted);
-  expectType<boolean>(app.closingRestartable);
-  expectType<(restartOpts?: unknown) => Promise<void>>(app.restart);
+  )
+  expectType<FastifyInstance<Http2Server>>(app)
+  expectType<boolean>(app.restarted)
+  expectType<boolean>(app.closingRestartable)
+  expectType<(restartOpts?: unknown) => Promise<void>>(app.restart)
 }
 
 {
-  const app = await restartable(createApplication, { logger: true }, fastify);
-  app.addPreRestartHook(
-    async (instance: FastifyInstance, restartOpts: unknown) => {}
-  );
-  app.addOnRestartHook(
-    async (instance: FastifyInstance, restartOpts: unknown) => {}
-  );
+  const app = await restartable(createApplication, { logger: true }, fastify)
+  app.addPreRestartHook(async (instance: FastifyInstance, restartOpts: unknown) => {})
+  app.addOnRestartHook(async (instance: FastifyInstance, restartOpts: unknown) => {})
 }
