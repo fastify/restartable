ea3e7047f62af1f2d021237023b21407cdea17ec
Frazer Smith <frazer.dev@icloud.com>
2024-12-12 20:31:48 +0000

    build(deps-dev): replace standard with neostandard (#81)
    * build(deps-dev): replace standard with neostandard
    
    * chore: add eslint.config.js
    
    * test(types): ignore unused import

--- types/index.test-d.ts
+++ types/index.test-d.ts
@@ -1,7 +1,8 @@
 import { fastify, FastifyInstance, FastifyServerOptions } from 'fastify'
 import { expectAssignable, expectType } from 'tsd'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars -- ApplicationFactory is used in commented out test
 import { restartable, ApplicationFactory } from './index'
-import type { Http2Server } from "http2"
+import type { Http2Server } from 'http2'
 
 type Fastify = typeof fastify
 
